4dab03b5a3ee1273647339eeee2637a96ba58de7
rizkisiraj <jomirwa@gmail.com>
2024-08-06 13:41:54 +0700

    Initial Commit

---
+++ SwipeableVideo/SwipeableVideoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwipeableVideoApp.swift
+//  SwipeableVideo
+//
+//  Created by Rizki Siraj on 06/08/24.
+//
+
+import SwiftUI
+
+@main
+struct SwipeableVideoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
